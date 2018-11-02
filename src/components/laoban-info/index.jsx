ed2bd18c27bcb15661270efb9e0633f3f2240f06
Hxc <hxc19980624@163.com>
2018-11-02 23:27:34 +0800

    收集老板页面表单数据

--- src/components/laoban-info/index.jsx
+++ src/components/laoban-info/index.jsx
@@ -4,15 +4,36 @@ import {NavBar, InputItem, Button, List, TextareaItem} from 'antd-mobile';
 import HeaderSelector from '../header-selector';
 
 class LaobanInfo extends Component {
+
+  state = {
+    header: '',
+    info: '',
+    post: '',
+    salary: '',
+    company: ''
+  };
+
+  handleChange = (name, val) => {
+    this.setState({
+      [name]: val
+    })
+  };
+
+  setHeader = header => {
+    this.setState({
+      header
+    })
+  };
+
   render () {
     return (
       <div>
         <NavBar>老板信息完善</NavBar>
-        <HeaderSelector/>
-        <InputItem>招聘职位:</InputItem>
-        <InputItem>公司名称:</InputItem>
-        <InputItem>职位薪资:</InputItem>
-        <TextareaItem title="职位要求" rows={3}/>
+        <HeaderSelector setHeader={this.setHeader}/>
+        <InputItem onChange={val => this.handleChange('post', val)}>招聘职位:</InputItem>
+        <InputItem onChange={val => this.handleChange('company', val)}>公司名称:</InputItem>
+        <InputItem onChange={val => this.handleChange('salary', val)}>职位薪资:</InputItem>
+        <TextareaItem title="职位要求" rows={3} onChange={val => this.handleChange('info', val)}/>
         <Button type="warning">保 存</Button>
       </div>
     )
